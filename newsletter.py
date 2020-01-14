8652c38c9a3883e20cf724f4d981f35532c32f24
yomajo <ed.vinas@yahoo.com>
2020-01-14 14:15:13 +0200

    no major changes, (un-/)comment for recent scrape.

--- newsletter.py
+++ newsletter.py
@@ -26,18 +26,18 @@ def get_base_urls_from_config(config_file_path, section_name):
 def main():
     base_urls = get_base_urls_from_config(config_file, 'BASE_URLS')
     # base_url_vz = base_urls[0]
-    # base_url_lrt = base_urls[1]
+    base_url_lrt = base_urls[1]
     # base_url_err = base_urls[2]
     # base_url_postimees = base_urls[3]
     # base_url_btimes = base_urls[4]
-    base_url_db = base_urls[5]
+    # base_url_db = base_urls[5]
 
     # vz_scrapper_inst = VzScrapper(base_url_vz, config_file, 'Output/vz_headlines.csv')
     # vz_headlines_list = vz_scrapper_inst.get_website_headlines_as_list()
     # vz_scrapper_inst.export_list_to_csv(vz_headlines_list, vz_scrapper_inst.output_csv_file)
-    # lrt_scrapper_inst = LrtScrapper(base_url_lrt, config_file, 'Output/lrt_headlines.csv')
-    # lrt_headlines_list = lrt_scrapper_inst.get_website_headlines_as_list()
-    # lrt_scrapper_inst.export_list_to_csv(lrt_headlines_list, lrt_scrapper_inst.output_csv_file)
+    lrt_scrapper_inst = LrtScrapper(base_url_lrt, config_file, 'Output/lrt_headlines.csv')
+    lrt_headlines_list = lrt_scrapper_inst.get_website_headlines_as_list()
+    lrt_scrapper_inst.export_list_to_csv(lrt_headlines_list, lrt_scrapper_inst.output_csv_file)
     # err_scrapper_inst = ERRScrapper(base_url_err, config_file, 'Output/err_headlines(no translaation yet).csv')
     # err_headlines_list = err_scrapper_inst.get_website_headlines_as_list()
     # err_scrapper_inst.export_list_to_csv(err_headlines_list, err_scrapper_inst.output_csv_file)
@@ -47,9 +47,9 @@ def main():
     # btimes_scrapper_inst = BalticTimesScrapper(base_url_btimes, config_file, 'Output/baltic_times_headlines.csv')
     # btimes_headlines_list = btimes_scrapper_inst.get_website_headlines_as_list()
     # btimes_scrapper_inst.export_list_to_csv(btimes_headlines_list, btimes_scrapper_inst.output_csv_file)
-    db_scrapper_inst = DbScrapper(base_url_db, config_file, 'Output/db_headlines.csv')
-    db_headlines_list = db_scrapper_inst.get_website_headlines_as_list()
-    db_scrapper_inst.export_list_to_csv(db_headlines_list, db_scrapper_inst.output_csv_file)
+    # db_scrapper_inst = DbScrapper(base_url_db, config_file, 'Output/db_headlines.csv')
+    # db_headlines_list = db_scrapper_inst.get_website_headlines_as_list()
+    # db_scrapper_inst.export_list_to_csv(db_headlines_list, db_scrapper_inst.output_csv_file)
 
 
 
