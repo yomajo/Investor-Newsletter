6efb47f137521c56a73303a8a10a0cc23ffc8f19
yomajo <ed.vinas@yahoo.com>
2020-01-21 11:44:30 +0200

    translation cls integration. Bugs preset (see manual.log)

--- newsletter.py
+++ newsletter.py
@@ -8,7 +8,8 @@ from translate import TranslateList
 from configparser import ConfigParser
 
 # GLOBAL VARIABLES:
-config_file = 'config.ini'
+CONFIG_FILE = 'config.ini'
+OUTPUT_HEADLINESDATA_FILE = 'Output/Headlines_data.csv'
 
 
 def get_base_urls_from_config(config_file_path, section_name):
@@ -34,36 +35,46 @@ def get_desired_langs(config_file_path, section_name):
     return desired_langs
 
 def main():
-    base_urls = get_base_urls_from_config(config_file, 'BASE_URLS')
-    # base_url_vz = base_urls[0]
-    base_url_lrt = base_urls[1]
-    # base_url_err = base_urls[2]
-    # base_url_postimees = base_urls[3]
-    # base_url_btimes = base_urls[4]
-    # base_url_db = base_urls[5]
+    base_urls = get_base_urls_from_config(CONFIG_FILE, 'BASE_URLS')
+    desired_langs = get_desired_langs(CONFIG_FILE, 'LANGUAGES')
+        
+    raw_scrappers_output = []
 
-    desired_langs = get_desired_langs(config_file, 'LANGUAGES')
-    print(desired_langs)
-    # vz_scrapper_inst = VzScrapper(base_url_vz, config_file, 'Output/vz_headlines.csv')
-    # vz_headlines_list = vz_scrapper_inst.get_website_headlines_as_list()
-    # vz_scrapper_inst.export_list_to_csv(vz_headlines_list, vz_scrapper_inst.output_csv_file)
-    # lrt_scrapper_inst = LrtScrapper(base_url_lrt, config_file, 'Output/lrt_headlines.csv')
-    # lrt_headlines_list = lrt_scrapper_inst.get_website_headlines_as_list()
-    # lrt_scrapper_inst.export_list_to_csv(lrt_headlines_list, lrt_scrapper_inst.output_csv_file)
-    # err_scrapper_inst = ERRScrapper(base_url_err, config_file, 'Output/err_headlines(no translaation yet).csv')
-    # err_headlines_list = err_scrapper_inst.get_website_headlines_as_list()
-    # err_scrapper_inst.export_list_to_csv(err_headlines_list, err_scrapper_inst.output_csv_file)
-    # postimees_scrapper_inst = PostimeesScrapper(base_url_postimees, config_file, 'Output/postimees_headlines(no translaation yet).csv')
-    # postimees_headlines_list = postimees_scrapper_inst.get_website_headlines_as_list()
-    # postimees_scrapper_inst.export_list_to_csv(postimees_headlines_list, postimees_scrapper_inst.output_csv_file)
-    # btimes_scrapper_inst = BalticTimesScrapper(base_url_btimes, config_file, 'Output/baltic_times_headlines.csv')
-    # btimes_headlines_list = btimes_scrapper_inst.get_website_headlines_as_list()
-    # btimes_scrapper_inst.export_list_to_csv(btimes_headlines_list, btimes_scrapper_inst.output_csv_file)
-    # db_scrapper_inst = DbScrapper(base_url_db, config_file, 'Output/db_headlines.csv')
-    # db_headlines_list = db_scrapper_inst.get_website_headlines_as_list()
-    # db_scrapper_inst.export_list_to_csv(db_headlines_list, db_scrapper_inst.output_csv_file)
+    # Scrape and output results into raw_scrappers_output list for each website
+    vz_scrapper_inst = VzScrapper(base_urls[0], CONFIG_FILE)
+    vz_headlines_list = vz_scrapper_inst.get_website_headlines_as_list()
+    raw_scrappers_output.append(vz_headlines_list)
 
+    lrt_scrapper_inst = LrtScrapper(base_urls[1], CONFIG_FILE)
+    lrt_headlines_list = lrt_scrapper_inst.get_website_headlines_as_list()
+    raw_scrappers_output.append(lrt_headlines_list)
 
+    err_scrapper_inst = ERRScrapper(base_urls[2], CONFIG_FILE)
+    err_headlines_list = err_scrapper_inst.get_website_headlines_as_list()
+    raw_scrappers_output.append(err_headlines_list)
+
+    postimees_scrapper_inst = PostimeesScrapper(base_urls[3], CONFIG_FILE)
+    postimees_headlines_list = postimees_scrapper_inst.get_website_headlines_as_list()
+    raw_scrappers_output.append(postimees_headlines_list)
+    
+    btimes_scrapper_inst = BalticTimesScrapper(base_urls[4], CONFIG_FILE)
+    btimes_headlines_list = btimes_scrapper_inst.get_website_headlines_as_list()
+    raw_scrappers_output.append(btimes_headlines_list)
+
+    db_scrapper_inst = DbScrapper(base_urls[5], CONFIG_FILE)
+    db_headlines_list = db_scrapper_inst.get_website_headlines_as_list()
+    raw_scrappers_output.append(db_headlines_list)
+
+    print('------COMPLETED SCRAPPING DATA TO LISTS, TRANSLATING, WRITING TO FILE START------')
+
+    for scrapped_list in raw_scrappers_output:
+        translator = TranslateList(scrapped_list, desired_langs)
+        try:
+            translated_headlines_data = translator.get_translated()
+            db_scrapper_inst.export_list_to_csv(translated_headlines_data, OUTPUT_HEADLINESDATA_FILE)
+        except:
+            continue
+    print('FINISHED')
 
 if __name__ == '__main__':
     main()
