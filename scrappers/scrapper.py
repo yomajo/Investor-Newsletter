a59ce05913d5edbb773f458502c42984d2298495
yomajo <ed.vinas@yahoo.com>
2020-02-10 12:31:09 +0200

    Added logging, improved docstrings, msg edits

--- scrappers/scrapper.py
+++ scrappers/scrapper.py
@@ -7,6 +7,10 @@ import os
 import requests
 import lxml
 import csv
+import logging
+
+# Initializing logging in module
+logger = logging.getLogger(__name__)
 
 # User Agent for Requests
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'}
@@ -37,7 +41,7 @@ class Scrapper():
         # Read contents under class named section in config.ini
         config = ConfigParser()
         config.read(self.config_file)
-        print(f'Reading config file {self.config_file} contents from section: {self.__class__.__name__.upper()}')        
+        logger.info(f'Reading config file {self.config_file} contents from section: {self.__class__.__name__.upper()}')        
         raw_cls_config = config.items(f'{self.__class__.__name__.upper()}')
         for _, cat in raw_cls_config:
             categs.append(cat)
@@ -57,7 +61,7 @@ class Scrapper():
         if r.status_code == 200:
             return r
         else:
-            Exception(f'Server did not respond well. Response code: {r.status_code} while accessing {url}')
+            logger.exception(f'Server did not respond well. Response code: {r.status_code} while accessing {url}')
 
     def scrape_category(self, response):
         '''---OVERWRITE WHEN INHERITING---'''
@@ -96,23 +100,23 @@ class Scrapper():
     def get_website_headlines_as_list(self):
         '''iterates over category urls, scrapes data from each category, returns a list of lists of unique headlines and urls'''
         self.get_urls()
-        print(f'Collected category urls:\n{self.urls_list}')
         self.unique_headlines_list = []
         for idx, url in enumerate(self.urls_list):
-            print(f'-----------New request in category: {self.categs[idx]}---------')
+            logger.info(f'New request for category: {self.categs[idx]}')
             response = self.get_response(url)
-            print(f'\nServer response time: {response.elapsed.total_seconds()}')
+            logger.info(f'Server response time: {response.elapsed.total_seconds()}')
             
             self.scrape_category(response)
             self.category_to_output_list(self.category_results, self.unique_headlines_list)
 
-            print('sleeping before jumping to next category... ZZZzzzz...')
+            logging.info('sleeping before jumping to next category... ZZzz...')
             sleep(randint(10, 50)/10)
         return self.unique_headlines_list
 
     def category_to_output_list(self, category_list, unique_records):
-        '''takes input arg of category-level scrape results and transfers unique records to output list. Returns it'''
-        print('Writing category headlines infomation to common variable')
+        '''takes input arg of category-level scrape results and transfers unique records to output list. Returns it
+        (prevents same entry from different categories)'''
+        logger.info('Writing category headlines infomation to common variable')
         for headline_info in category_list:
             if headline_info not in unique_records:
                 unique_records.append(headline_info)
