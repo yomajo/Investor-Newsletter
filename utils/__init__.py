dcdeb24ed721bcdbdb1143ec4afda4b934fcb55e
yomajo <ed.vinas@yahoo.com>
2020-02-12 23:14:34 +0200

    utils __init__ updates with new functions import

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,8 @@
 from .utils import export_list_to_csv
 from .utils import get_headline_urls_in_db 
 from .utils import get_headlines_not_in_db
-# from .utils import get_user_agent_dict 
+from .utils import get_user_agent_dict
+from .utils import get_user_agent_str
+from .utils import get_working_proxy
+from .utils import csv_contents_to_list
+from .constants import USER_AGENTS, SERVICE_URLS
