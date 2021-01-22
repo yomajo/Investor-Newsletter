4847ddeca4a6c95c821afeac490a858abef8a782
yomajo <ed.vinas@yahoo.com>
2021-01-22 14:52:48 +0200

    refactored to download images, other minor edits

--- scrappers/lrt_scrapper.py
+++ scrappers/lrt_scrapper.py
@@ -1,4 +1,5 @@
 from scrappers.scrapper import Scrapper
+from utils import download_img
 from bs4 import BeautifulSoup
 import logging
 
@@ -9,32 +10,49 @@ class LrtScrapper(Scrapper):
     '''Modifying inherited structure to scrape categories within LrtScrapper in config.ini. Instance takes args:
     - base website
     - path to config file where class takes website categories suffix to url
-    - relative output csv file path'''
+    LRT does not have feature articles, therefore no corresponding call to get_feature_article within category'''
         
-    def get_category_articles(self, content_container, appendable_output_list):
-        '''appends second arg with list of article headlines and urls from passed content container'''
-        try:
-            # all_category_divs = content_container.find('div', id='category_list')
-            article_containers = content_container.findAll('h3', class_='news__title')
-            for article in article_containers:
-                article_headline = article.a.text.strip()
-                article_url_unval = article.a['href']
+    def get_category_articles(self, content_container:object, category:str):
+        '''scrapes articles data within category content_container if urls not yet in self.unique_urls_set.
+        Appends headline data dicts to self.website_data list'''
+        try:    
+            all_category_divs = content_container.findAll('div', class_='news')
+            for article_div in all_category_divs:
+                # article url
+                article_url_unval = article_div.h3.a['href']
                 article_url = self.validate_url(article_url_unval)
-                cycle_output_as_list = [article_headline, article_url]
-                appendable_output_list.append(cycle_output_as_list)
+                
+                if article_url not in self.unique_urls_set:
+                    self.unique_urls_set.add(article_url)
+                    # headline
+                    article_headline = article_div.h3.a.text.strip()    
+                    
+                    # article img url
+                    try:
+                        img_url_raw = article_div.img['data-src']
+                        article_img_url = self.validate_url(img_url_raw)
+                        img_path = download_img(article_img_url, self.user_agent)
+                    except TypeError:
+                        logger.debug(f'Could not get img url for this article: {article_url} Returning \'#N/A\'')
+                        img_url = img_path = '#N/A'
+                    headline_data_dict = {
+                                'headline':article_headline,
+                                'category':category,
+                                'url':article_url,
+                                'img_url':article_img_url,
+                                'img_path':img_path}
+                    self.website_data.append(headline_data_dict)
         except:
             logger.exception('Error grabbing category articles. Check for WEBSITE STRUCTURE CHANGES')
 
-    def scrape_category(self, response):
-        '''Collects links and article headlines within passed category response'''
+    def scrape_category(self, response:object, category:str):
+        '''Collects headline data to cls variable self.website_data for passed response obj (category specific)'''
         if response != None:
-            self.category_results = []
-            # Pass corresponding html element and CSS class to  
-            self.content_container = self.get_content_container(response, 'div', 'news-list')
-            self.get_category_articles(self.content_container, self.category_results)
+            content_container = self.get_content_container(response, 'div', 'news-list')
+            self.get_category_articles(content_container, category)
         else:
             logger.error('Server did not respond well')
-        
+
 
 if  __name__ == '__main__':
     pass
